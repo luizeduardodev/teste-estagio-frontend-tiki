657906fdfbe44400853b13c620c31a8cdf985f32
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-09-30 16:21:05 -0300

    App.jsx refatorado e melhorado

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import Carousel from "./components/Carousel/Carousel";
-import Form from "./components/Form/Form";
-import Footer from "./components/Footer/Footer";
+import MainContent from "./components/MainContent/MainContent";
 
-import brand from "./assets/svg/logo-tiki.svg";
-import titleTwo from "./assets/svg/tittle2.svg";
-import iconYellow from "./assets/svg/icon-yellow.svg";
-
-import "./styles/App.scss";
+import "./styles/app.scss";
 
 const App = () => {
     return (
         <>
-            <section className="container-general">
-                <div className="wrapper">
-                    <header className="header">
-                        <img src={brand} alt="Logo da Tiki" className="brand" />
-                    </header>
-                    <Carousel />
-                    <img
-                        src={titleTwo}
-                        alt="Imagem quero fazer parte"
-                        className="quero-fazer-parte"
-                    />
-                    <img
-                        src={iconYellow}
-                        alt="Icone com vários + amarelos"
-                        className="icon-yelloww"
-                    />
-                    <Form />
-                    <Footer />
-                </div>
-            </section>
+            <MainContent />
         </>
     );
 };
