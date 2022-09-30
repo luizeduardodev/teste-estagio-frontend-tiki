b7f29b2b4d5d1616abee38dbe58ce6c21aaeab64
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-09-30 10:20:33 -0300

    Adição do Footer component e melhorias no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Carousel from "./components/Carousel/Carousel";
 import Form from "./components/Form/Form";
+import Footer from "./components/Footer/Footer";
 
 import brand from "./assets/svg/logo-tiki.svg";
 import titleTwo from "./assets/svg/tittle2.svg";
@@ -9,31 +10,28 @@ import "./styles/App.scss";
 
 const App = () => {
     return (
-        <div className="container-general">
-            <div className="wrapper">
-                <img src={brand} alt="Logo Tiki" className="brand" />
-                <Carousel />
-                <img src={titleTwo} alt="" />
-                <img src={iconYellow} alt="" />
-                <Form />
-
-                <footer className="container-footer">
-                    <h3>Texto Legal</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        A, enim repudiandae! Quos nostrum voluptate laborum
-                        impedit magnam ut! Veniam deleniti in fuga, ad
-                        dignissimos ipsum doloremque quisquam omnis quos nemo?
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        A, enim repudiandae! Quos nostrum voluptate laborum
-                        impedit magnam ut! Veniam deleniti in fuga, ad
-                        dignissimos ipsum doloremque quisquam omnis quos nemo?
-                    </p>
-                </footer>
-            </div>
-        </div>
+        <>
+            <section className="container-general">
+                <div className="wrapper">
+                    <header className="header">
+                        <img src={brand} alt="Logo da Tiki" className="brand" />
+                    </header>
+                    <Carousel />
+                    <img
+                        src={titleTwo}
+                        alt="Imagem quero fazer parte"
+                        className="quero-fazer-parte"
+                    />
+                    <img
+                        src={iconYellow}
+                        alt="Icone com vários + amarelos"
+                        className="icon-yelloww"
+                    />
+                    <Form />
+                    <Footer />
+                </div>
+            </section>
+        </>
     );
 };
 
