ef15f5ff97b3c7921a14525e41af8081a4aeb817
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-09-30 16:23:15 -0300

    Loading adicionado, pequenas alterações no jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,30 +1,83 @@
-const Form = () => {
-    return (
-        <div className="container-general">
-            <h2>Cadastre-se e fique por dentro das novidades</h2>
+import { useState } from "react";
+import Loading from "../Loading/Loading";
 
-            <form className="container-form">
+import "./style.scss";
+
+const Form = () => {
+    const [button, setButton] = useState(false);
+    const [valueInputText, setValueInputText] = useState("");
+    const [valueInputEmail, setValueInputEmail] = useState("");
+    const [checkbox, setCheckbox] = useState(false);
+
+    function clickCheckbox() {
+        setCheckbox(!checkbox);
+    }
+
+    const handleButton = (e) => {
+        e.preventDefault();
+
+        setButton(!button);
+
+        setTimeout(() => {
+            setButton(button);
+            setCheckbox(!checkbox);
+            setValueInputText("");
+            setValueInputEmail("");
+        }, 800);
+    };
+
+    const clearInputText = (e) => {
+        setValueInputText(e.target.value);
+    };
+
+    const clearInputsEmail = (e) => {
+        setValueInputEmail(e.target.value);
+    };
+
+    return (
+        <section id="form">
+            <form className="container-form" onSubmit={handleButton}>
+                <h2>Cadastre-se e fique por dentro das novidades</h2>
+
                 <input
                     type="text"
                     placeholder="Nome"
                     className="design-input"
+                    onChange={clearInputText}
+                    value={valueInputText}
                 />
+
                 <input
                     type="email"
                     placeholder="meuemail@gmail.com"
                     className="design-input"
+                    onChange={clearInputsEmail}
+                    value={valueInputEmail}
+                    disabled={valueInputText === "" && true}
                 />
 
                 <div className="container-checkbox">
-                    <input type="checkbox" id="privacyPolicy" />
-                    <label htmlFor="privacyPolicy">
-                        Declaro que li e aceito a política de privacidade
-                    </label>
+                    <div className="checkbox">
+                        <input
+                            type="checkbox"
+                            id="privacy-policy"
+                            required
+                            onClick={clickCheckbox}
+                            disabled={valueInputEmail == false && true}
+                            checked={!checkbox == false && true}
+                        />
+
+                        <label htmlFor="privacy-policy">
+                            Declaro que li e aceito a política de privacidade
+                        </label>
+                    </div>
                 </div>
 
-                <button type="submit">Enviar</button>
+                <button type="submit" disabled={!checkbox}>
+                    {button ? <Loading /> : "Enviar"}
+                </button>
             </form>
-        </div>
+        </section>
     );
 };
 
