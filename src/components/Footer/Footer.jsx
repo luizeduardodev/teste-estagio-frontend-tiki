9132756b20e3f78fc5f4ef89e872cc08e680b004
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-09-30 10:22:55 -0300

    component Footer e style.scss adicionado

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="container-footer">
+            <h3>Texto Legal</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, enim
+                repudiandae! Quos nostrum voluptate laborum impedit magnam ut!
+                Veniam deleniti in fuga, ad dignissimos ipsum doloremque
+                quisquam omnis quos nemo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eveniet consequatur voluptas, magnam culpa ipsa debitis molestias maiores reiciendis rem recusandae.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, enim
+                repudiandae! Quos nostrum voluptate laborum impedit magnam ut!
+                Veniam deleniti in fuga, ad dignissimos ipsum doloremque
+                quisquam omnis quos nemo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi adipisci vitae quaerat ratione fugit, accusamus quas? Nulla inventore asperiores sit
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
