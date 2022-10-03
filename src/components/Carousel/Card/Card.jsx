5b7c46b7f6edfddf3f9d99373529a3cf941e896d
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-10-03 10:23:33 -0300

    Pequenas alteações

--- src/components/Carousel/Card/Card.jsx
+++ src/components/Carousel/Card/Card.jsx
@@ -4,28 +4,26 @@ import "./style.scss";
 const Card = ({ title, image, className }) => {
     const [isActive, setIsActive] = useState(false);
 
-    const handleClick = () => {
+    const handleImage = (event) => {
+        const img = document.getElementById("img");
+        const srcImage = event.target.src;
+
+        img.setAttribute("src", srcImage);
         setIsActive(!isActive);
     };
 
     let classActive = isActive ? "active" : "";
 
-    const handleImage = (event) => {
-        const img = document.getElementById("img");
-        const srcImage = event.target.src;
-        const lock = "http://127.0.0.1:5173/src/assets/svg/lock.svg";
-
-        return srcImage == undefined || srcImage == lock
-            ? alert("Evento ainda não disponivel!")
-            : img.setAttribute("src", srcImage);
-    };
-
     return (
-        <div className={className} onClick={handleImage}>
+        <div className={className}>
             <div className="container-img">
-                <img src={image} className={`${classActive}`} onClick={handleClick} />
+                <img
+                    src={image}
+                    className={classActive}
+                    onClick={handleImage}
+                />
             </div>
-            <p className={`${classActive}`}>{title}</p>
+            <p className={classActive}>{title}</p>
         </div>
     );
 };
